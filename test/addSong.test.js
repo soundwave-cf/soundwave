87d343212d26d5deeed941a2b587ad98221c7fa7
cody <codyjeromegreen@gmail.com>
2018-04-11 23:31:29 -0700

    fixed addSong test

--- test/addSong.test.js
+++ test/addSong.test.js
@@ -6,21 +6,22 @@ const server = require('../server.js');
 require('dotenv').config();
 
 describe('All tests', () => {
-  // beforeAll(server.start);
-  // afterAll(server.stop);
-  // describe('Server tests', () => {
-  //   test('throws 404 if route is not found', (done) => {
-  //     expect(40).toEqual(40);
-  //     done();
-  //   });
-  // });
-
+  beforeAll(server.start);
+  afterAll(server.stop);
+  describe('Server tests', () => {
+    test('throws 404 if route is not found', (done) => {
+      expect(40).toEqual(40);
+      done();
+    });
+  });
+  //here make sure you dont test this user in postman
+  //just look at the db in heroku labs
   describe('Add a Song', () => {
-    let correctToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YWNlYzJlY2ZhMTFmNjU2MGY5OTVlMGQiLCJpYXQiOjE1MjM1MDYzOTN9.npnPqPt-nJnzikZLMoUMQWSxA5BvKbzW-gO6m2kr38c';
+    let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YWNlYzJlY2ZhMTFmNjU2MGY5OTVlMGQiLCJpYXQiOjE1MjM1MTQzNTN9.nBhPBqhh54kYzB0JGfPcVkm1SSRILXx-o8bA5ps9bBI';
 
     it('User should be able to add song with correct token', (done) => {
       return superagent.post(SERVER_URL + '/addSong')
-        .set('Authorization', `Bearer ${correctToken}`)
+        .set('Authorization', `Bearer ${token}`)
         .send({
           username: 'jest',
           artist: 'The Pixies',
